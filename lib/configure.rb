c52c250b9d262fb59105245ada6eac6a42844290
gremerritt <gremerritt@gmail.com>
2016-06-17 18:30:57 -0400

    adds setting dropbox root to config

--- lib/configure.rb
+++ lib/configure.rb
@@ -1,6 +1,7 @@
 require "templates"
 require "yaml"
 require "dropbox_sdk"
+require "json"
 
 module Henchman
   def self.configure
@@ -23,10 +24,20 @@ module Henchman
       end
     end
 
+    begin
+      client = DropboxClient.new(config[:dropbox][:access_token])
+      account_info = client.account_info()
+      puts "Successfully connected to Dropbox: "
+      puts "  #{account_info['display_name']} [#{account_info['email']}]"
+    rescue StandardError => err
+      puts "Error connecting to Dropbox account (#{err}). Try deleting the "\
+           "henchman configuration file (`rm ~/.henchman`) and rerunning"\
+           "`henchman configure`"
+      return
+    end
+
     if config[:dropbox][:root].empty? || agree()
-      begin
-        get_dropbox_root config
-      end
+      get_dropbox_root config, client
     end
 
     File.open(config_file, "w") { |f| f.write( config.to_yaml ) }
@@ -66,8 +77,37 @@ module Henchman
     end
   end
 
-  def self.get_dropbox_root config
-    client = DropboxClient.new(config[:dropbox][:access_token])
-    puts "linked account:", client.account_info().inspect
+  def self.get_dropbox_root(config, client)
+    # paths = Hash.new
+    # build_dropbox_dirs(paths, client, '/', 0)
+    not_done = true
+    while not_done
+      path = ask("Enter the path to your music directory in Dropbox: (? for help)" )
+      if path == '?'
+        puts "The path to your music directory is a unix-like path. For example: "\
+             "/Some/Directory/Music"
+        next
+      end
+
+      begin
+        metadata = client.metadata(path.chomp!('/'))
+        config[:dropbox][:root] = path
+        puts "Valid path!"
+        not_done = false
+      rescue StandardError => err
+        print "Invalid path. "
+        not_done = agree("Try again? (y/n) ")
+      end
+    end
   end
+
+  # def self.build_dropbox_dirs(paths, client, path, level)
+  #   return if level == 2
+  #   metadata = client.metadata(path)
+  #   metadata['contents'].each do |elem|
+  #     next if !elem['is_dir']
+  #     paths[elem['path']] = Hash.new
+  #     build_dropbox_dirs(paths[elem['path']], client, elem['path'], level+1)
+  #   end
+  # end
 end
