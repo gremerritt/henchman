4a4e3ab61842d1edd3fa24f1494d12037d918a4b
Greg Merritt <gremerritt@gmail.com>
2016-06-22 13:47:31 -0400

    makes applescript scripts their own fxns

--- lib/applescript.rb
+++ lib/applescript.rb
@@ -2,52 +2,61 @@ module Henchman
 
   class AppleScript
 
-    def initialize
-      @script_get_active_app = "tell application \"System Events\"\n"\
-  	                           "  set activeApp to name of first application process whose frontmost is true\n"\
-  	                           "  return activeApp\n"\
-                               "end tell"
-    end
-
     def setup config
       @delimiter = config[:delimiter]
-      @script_get_selection = "tell application \"iTunes\"\n"\
-                              "  try\n"\
-                              "    if class of selection as string is \"file track\" then\n"\
-                              "      set data_artist to artist of selection as string\n"\
-                              "      set data_album to album of selection as string\n"\
-                              "      set data_title to name of selection as string\n"\
-                              "      set data_location to POSIX path of (location of selection as string)\n"\
-                              "      set str to data_artist & \"#{@delimiter}\" & "\
-                              "                 data_album  & \"#{@delimiter}\" & "\
-                              "                 data_title  & \"#{@delimiter}\" & "\
-                              "                 data_location\n"\
-                              "      return str\n"\
-                              "    end if\n"\
-                              "  on error\n"\
-                              "    return \"\"\n"\
-                              "  end try\n"\
-                              "end tell"
-      @script_prompt = "tell application \"iTunes\"\n"\
-                       "  display dialog \"Fetch?\"\n"\
-                       "end tell"
-      @update_track_location_temp = "tell application \"iTunes\"\n"\
-                                    "  try\n"\
-                                    "    set data_tracks to "\
-                                    "      (every track whose artist is \"{ITUNES_ARTIST}\" and "\
-                                    "                         album  is \"{ITUNES_ALBUM}\"  and "\
-                                    "                         name   is \"{ITUNES_NAME}\")\n"
-                                    "    if (count of data_tracks) is 1 then\n"\
-                                    "      set location of (item 1 of data_tracks) to "\
-                                    "        (POSIX file \"{LOCAL_FILE})\"\n"\
-                                    "      return 1\n"\
-                                    "    else\n"\
-                                    "      return 0\n"\
-                                    "    end if\n"\
-                                    "  on error\n"\
-                                    "    return 0\n"\
-                                    "  end try\n"\
-                                    "end tell"
+    end
+
+    def get_active_app_script
+      "tell application \"System Events\"\n"\
+  	  "  set activeApp to name of first application process whose frontmost is true\n"\
+  	  "  return activeApp\n"\
+      "end tell"
+    end
+
+    def prompt_script
+      "tell application \"iTunes\"\n"\
+      "  display dialog \"Fetch?\"\n"\
+      "end tell"
+    end
+
+    def update_track_location_script selection, local_file
+      "tell application \"iTunes\"\n"\
+      "  try\n"\
+      "    set data_tracks to "\
+      "      (every track whose artist is \"#{selection[:artist]}\" and "\
+      "                         album  is \"#{selection[:album]}\"  and "\
+      "                         name   is \"#{selection[:track]}\")\n"
+      "    if (count of data_tracks) is 1 then\n"\
+      "      set location of (item 1 of data_tracks) to "\
+      "        (POSIX file \"#{local_file}\")\n"\
+      "      return 1\n"\
+      "    else\n"\
+      "      return 0\n"\
+      "    end if\n"\
+      "  on error\n"\
+      "    return 0\n"\
+      "  end try\n"\
+      "end tell"
+    end
+
+    def get_selection_script
+      "tell application \"iTunes\"\n"\
+      "  try\n"\
+      "    if class of selection as string is \"file track\" then\n"\
+      "      set data_artist to artist of selection as string\n"\
+      "      set data_album to album of selection as string\n"\
+      "      set data_title to name of selection as string\n"\
+      "      set data_location to POSIX path of (location of selection as string)\n"\
+      "      set str to data_artist & \"#{@delimiter}\" & "\
+      "                 data_album  & \"#{@delimiter}\" & "\
+      "                 data_title  & \"#{@delimiter}\" & "\
+      "                 data_location\n"\
+      "      return str\n"\
+      "    end if\n"\
+      "  on error\n"\
+      "    return \"\"\n"\
+      "  end try\n"\
+      "end tell"
     end
 
     def get_album_tracks_script artist, album
@@ -75,7 +84,7 @@ module Henchman
     end
 
     def get_selection ret
-      selection = %x( #{applescript_command(@script_get_selection)} ).chomp
+      selection = %x( #{applescript_command(get_selection_script)} ).chomp
       info = selection.split(@delimiter)
       if info.empty?
         false
@@ -92,25 +101,22 @@ module Henchman
     def get_album_tracks selection
       artist = selection[:artist]
       album  = selection[:album]
-      tracks = %x( #{applescript_command(get_album_tracks_script artist, album)} ).chomp
+      tracks = %x(#{applescript_command(get_album_tracks_script artist, album)}).chomp
       tracks = tracks.split(@delimiter)
       tracks.delete selection[:track]
       tracks
     end
 
     def fetch?
-      (%x(#{applescript_command(@script_prompt)}).chomp == "button returned:OK") ? true : false
+      (%x(#{applescript_command(prompt_script)}).chomp == "button returned:OK") ? true : false
     end
 
     def get_active_app
-      %x( #{applescript_command(@script_get_active_app)} ).chomp
+      %x(#{applescript_command(get_active_app_script)}).chomp
     end
 
     def set_track_location
-      update_track_location = @update_track_location_temp.gsub("{ITUNES_ARTIST}", "i_art")
-                                                         .gsub("{ITUNES_ALBUM}", "i_alb")
-                                                         .gsub("{ITUNES_NAME}", "i_name")
-                                                         .gsub("{LOCAL_FILE}", "")
+      ret = %x(#{applescript_command(update_track_location_script selection, local_file)}).chomp
       puts update_track_location
     end
 
