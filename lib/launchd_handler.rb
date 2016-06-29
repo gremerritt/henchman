da3ed901fdc8c242b385396755d3d0711f6d31d9
Greg Merritt <gremerritt@gmail.com>
2016-06-29 18:56:41 -0400

    Add a launchd daemon for cleanup

--- lib/launchd_handler.rb
+++ lib/launchd_handler.rb
@@ -13,28 +13,39 @@ module Henchman
         return if !Henchman.connect
       end
 
-      puts "Creating agent"
-      plist      = Henchman::Templates.plist
-      plist_path = File.expand_path("~/Library/LaunchAgents/com.henchman.plist")
-      shell_script_path = File.expand_path("~/.henchman/run.sh")
+      puts "Creating agents"
+      plist_main       = Henchman::Templates.plist_main
+      plist_path_main  = File.expand_path("~/Library/LaunchAgents/com.henchman.plist")
+      plist_clean      = Henchman::Templates.plist_clean
+      plist_path_clean = File.expand_path("~/Library/LaunchAgents/com.henchman.clean.plist")
+      shell_script_path_main  = File.expand_path("~/.henchman/run.sh")
+      shell_script_path_clean = File.expand_path("~/.henchman/clean.sh")
       cache_path        = File.expand_path("~/.henchman/cache")
-      File.write(plist_path, plist)
-      File.write(shell_script_path, Henchman::Templates.shell_script)
+      File.write(plist_path_main, plist_main)
+      File.write(shell_script_path_main, Henchman::Templates.shell_script('run'))
+      File.write(plist_path_clean, plist_clean)
+      File.write(shell_script_path_clean, Henchman::Templates.shell_script('clean'))
       File.open(cache_path, "w") { |f| f.write( Henchman::Templates.cache.to_yaml ) }
 
       puts "Launching agent"
-      `chmod +x #{shell_script_path}`
-      `launchctl load #{plist_path}`
+      `chmod +x #{shell_script_path_main}`
+      `chmod +x #{shell_script_path_clean}`
+      `launchctl load #{plist_path_main}`
+      `launchctl load #{plist_path_clean}`
 
-      puts "Launched successful! You are now running henchman."
+      puts "Launched successfully! You are now running henchman."
     end
 
     def self.stop
       puts "Stopping agents"
-      plist_path = File.expand_path("~/Library/LaunchAgents/com.henchman.plist")
-      `launchctl unload #{plist_path}`
-      `rm #{plist_path}`
+      plist_path_main = File.expand_path("~/Library/LaunchAgents/com.henchman.plist")
+      plist_path_clean = File.expand_path("~/Library/LaunchAgents/com.henchman.clean.plist")
+      `launchctl unload #{plist_path_main}`
+      `launchctl unload #{plist_path_clean}`
+      `rm #{plist_path_main}`
+      `rm #{plist_path_clean}`
       `rm #{File.expand_path("~/.henchman/run.sh")}`
+      `rm #{File.expand_path("~/.henchman/clean.sh")}`
       `rm #{File.expand_path("~/.henchman/cache")}`
       `rm #{File.expand_path("~/.henchman/stdout.log")}`
       `rm #{File.expand_path("~/.henchman/stderr.log")}`
--- lib/templates.rb
+++ lib/templates.rb
@@ -1,6 +1,6 @@
 module Henchman
   class Templates
-    def self.plist
+    def self.plist_main
       config = YAML.load_file(File.expand_path('~/.henchman/config'))
 
       "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"\
@@ -24,9 +24,45 @@ module Henchman
       "</plist>"
     end
 
-    def self.shell_script
+    def self.plist_clean
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"\
+      "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" "\
+      "\"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n"\
+      "<plist version=\"1.0\">\n"\
+      "<dict>\n"\
+      "\t<key>Label</key>\n"\
+      "\t<string>henchman_clean</string>\n"\
+      "\t<key>ProgramArguments</key>\n"\
+      "\t<array>\n"\
+      "\t\t<string>#{File.expand_path('~/.henchman/clean.sh')}</string>\n"\
+      "\t</array>\n"\
+      "\t<key>StartCalendarInterval</key>\n"\
+      "\t<array>\n"\
+      "\t<!-- Run twice a day. At 12:00 AM and at 12:00 PM -->\n"\
+      "\t\t<dict>\n"\
+      "\t\t\t<key>Hour</key>\n"\
+      "\t\t\t<integer>00</integer>\n"\
+      "\t\t\t<key>Minute</key>\n"\
+      "\t\t\t<integer>00</integer>\n"\
+      "\t\t</dict>\n"\
+      "\t\t<dict>\n"\
+      "\t\t\t<key>Hour</key>\n"\
+      "\t\t\t<integer>12</integer>\n"\
+      "\t\t\t<key>Minute</key>\n"\
+      "\t\t\t<integer>00</integer>\n"\
+      "\t\t</dict>\n"\
+      "\t</array>\n"\
+      "\t<key>StandardOutPath</key>\n"\
+      "\t<string>#{File.expand_path('~/.henchman/stdout.log')}</string>\n"\
+      "\t<key>StandardErrorPath</key>\n"\
+      "\t<string>#{File.expand_path('~/.henchman/stderr.log')}</string>\n"\
+      "</dict>\n"\
+      "</plist>"
+    end
+
+    def self.shell_script command
       "#!/bin/sh\n"\
-      "#{`which henchman`.chomp} run"
+      "#{`which henchman`.chomp} #{command}"
     end
 
     def self.config
